d2f3183b6450aadf9d2165e4e38e52ff9df30cee
HeYuJie <992565660@qq.com>
2020-08-06 10:58:59 +0800

    Revert "Develop new  Push"
    This reverts commit bb502293

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -12,7 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("test","Hello, World, I am develop");
         Log.d("test","Hello, World, I am develop");
-
-        Log.d("test","Hello, World, I am develop new ");
     }
 }
