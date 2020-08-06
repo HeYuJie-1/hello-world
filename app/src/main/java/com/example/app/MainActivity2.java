424afca64896f17a1ab71842da156e96755f4b4e
HeYuJie <992565660@qq.com>
2020-08-06 10:52:36 +0800

    Develop2 Push

--- app/src/main/java/com/example/app/MainActivity2.java
+++ app/src/main/java/com/example/app/MainActivity2.java
@@ -2,6 +2,7 @@ package com.example.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Log.d("test","wo yao");
+        Log.d("test","ba ni");
+        Log.d("test","gai de");
+        Log.d("test","mian mu quan fei");
     }
 }
