ff37966bf4ce2c119eab6924e42235e58f0a0809
HeYuJie <992565660@qq.com>
2020-08-07 08:36:42 +0800

    Develop 修改

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("test","Hello, World, I am develop");
-        Log.d("test","Hello, World, I am develop");
+        Log.d("test","develop 修改");
     }
 }
